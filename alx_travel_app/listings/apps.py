769e90eddd8c9adab2fd253c1d5400c1b9b83c75
joekariuki3 <joekariuki3@gmail.com>
2025-07-18 09:49:20 +0300

    Update `listings` app structure and refactor settings
    - Renamed `alx_travel_app.listings` to `listings`.
    - Updated app references in settings and models.
    - Reorganized `listings` app files:
      - Migrated models, serializers, management commands, and tests.
    - Improved modularity and aligned with Django conventions.

--- alx_travel_app/listings/apps.py
+++ alx_travel_app/listings/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class ListingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'alx_travel_app.listings'
+    name = 'listings'
 
