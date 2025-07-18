538435b5a8c3671c4f02632c0c5cbcf8ccbad2ea
joekariuki3 <joekariuki3@gmail.com>
2025-07-18 13:32:11 +0300

    Update serializers to include `location`, `host`, `guest`, and `listing` fields as read-only

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -4,11 +4,11 @@ from .models import Listing, Booking
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = ['id', 'title', 'price_per_night', 'description', 'image_url', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'price_per_night', 'description', 'image_url', 'location', 'host', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'location', 'host', 'created_at', 'updated_at']
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = ['id', 'start_date', 'end_date', 'total_price', 'status', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        fields = ['id', 'start_date', 'end_date', 'total_price', 'status', 'guest', 'listing', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'guest', 'listing','created_at', 'updated_at']
